18d3e8ce712289dce04081f84cdb5f7b1a052459
ElisaCapololo <elisacapololo2017@gmail.com>
2019-01-15 12:36:59 +0000

    Add a new thinks in trips and google map

---
+++ app/src/main/java/com/elisacapololo/bustrip/MyAdapter.java
@@ -0,0 +1,71 @@
+package com.elisacapololo.bustrip;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.TripsViewHolder> {
+
+    private Context mContext;
+    private List<Trips> tripsList;
+
+    public MyAdapter(Context mContext, List<Trips> tripsList) {
+        this.mContext = mContext;
+        this.tripsList = tripsList;
+    }
+
+    @NonNull
+    @Override
+    public TripsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View v = inflater.inflate(R.layout.item_bus, viewGroup, false);
+        return new TripsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TripsViewHolder tripsViewHolder, int i) {
+        tripsViewHolder.cityFrom.setText(tripsList.get(i).getCity_form());
+        tripsViewHolder.cityDestination.setText(tripsList.get(i).getCity_destination());
+        tripsViewHolder.partida.setText(tripsList.get(i).getPartida());
+        tripsViewHolder.chegada.setText(tripsList.get(i).getChegada());
+        tripsViewHolder.preco.setText(tripsList.get(i).getPreco());
+        tripsViewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, InformationTripsActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tripsList.size();
+    }
+
+    public static class TripsViewHolder extends RecyclerView.ViewHolder {
+        TextView cityFrom;
+        TextView cityDestination;
+        TextView partida;
+        TextView chegada;
+        TextView preco;
+        CardView cardView;
+        public TripsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cityFrom = itemView.findViewById(R.id.city_from);
+            cityDestination = itemView.findViewById(R.id.city_destination);
+            partida = itemView.findViewById(R.id.hour_partida);
+            chegada = itemView.findViewById(R.id.hour_chegada);
+            preco = itemView.findViewById(R.id.money);
+            cardView = itemView.findViewById(R.id.cardview_id);
+        }
+    }
+}
---
+++ app/src/main/java/com/elisacapololo/bustrip/Trips.java
@@ -0,0 +1,60 @@
+package com.elisacapololo.bustrip;
+
+public class Trips {
+
+   private String city_form;
+   private String city_destination;
+   private String partida;
+   private String chegada;
+   private String preco;
+
+    public Trips() {}
+
+    public Trips(String city_form, String city_destination, String partida, String chegada, String preco) {
+        this.city_form = city_form;
+        this.city_destination = city_destination;
+        this.partida = partida;
+        this.chegada = chegada;
+        this.preco = preco;
+    }
+
+    public String getCity_form() {
+        return city_form;
+    }
+
+    public void setCity_form(String city_form) {
+        this.city_form = city_form;
+    }
+
+    public String getCity_destination() {
+        return city_destination;
+    }
+
+    public void setCity_destination(String city_destination) {
+        this.city_destination = city_destination;
+    }
+
+    public String getPartida() {
+        return partida;
+    }
+
+    public void setPartida(String partida) {
+        this.partida = partida;
+    }
+
+    public String getChegada() {
+        return chegada;
+    }
+
+    public void setChegada(String chegada) {
+        this.chegada = chegada;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+}
