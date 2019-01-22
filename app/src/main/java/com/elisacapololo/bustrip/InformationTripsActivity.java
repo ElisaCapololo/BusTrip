d8d00f35d8fb95a358f26699befa04d15de46a3b
ElisaCapololo <elisacapololo2017@gmail.com>
2019-01-22 14:40:37 +0000

    Funcionando o icone de voltar

--- app/src/main/java/com/elisacapololo/bustrip/InformationTripsActivity.java
+++ app/src/main/java/com/elisacapololo/bustrip/InformationTripsActivity.java
@@ -1,7 +1,10 @@
 package com.elisacapololo.bustrip;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class InformationTripsActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class InformationTripsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_trips);
         getSupportActionBar().setTitle("Informações da Viagem");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
